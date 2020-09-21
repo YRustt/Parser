fbc2c678e585ee60285bf3ed1f963f16f64269ca
Rustam Yunusov <yrustt@yandex.ru>
2020-09-21 06:33:48 +0300

    Added new files

--- src/main.py
+++ src/main.py
@@ -14,29 +14,29 @@ if __name__ == "__main__":
     #     writer.writerows(parser.parse())
 
 
-    # with open(CATEGORIES_PATH, "r") as f:
-    #     reader = csv.DictReader(f, delimiter="\t")
-    #     for i in range(29):
-    #         print(next(reader))
-    #
-    #     for category in reader:
-    #         category = Category(**category)
-    #         category_parser = CategoryDetailParser(category)
-    #         filename = get_product_urls_filepath(category.category_name, category.subcategory_name)
-    #         with CSVWriter(filename, Url) as writer:
-    #             writer.writerows(category_parser.parse())
-
-
     with open(CATEGORIES_PATH, "r") as f:
         reader = csv.DictReader(f, delimiter="\t")
+        for i in range(30):
+            print(next(reader))
 
         for category in reader:
-            urls_filename = get_product_urls_filepath(category["category_name"], category["subcategory_name"])
-            filename = get_products_filepath(category["category_name"], category["subcategory_name"])
-            with open(urls_filename, "r") as f2, CSVWriter(filename, Product) as writer:
-                reader2 = csv.DictReader(f2, delimiter="\t")
+            category = Category(**category)
+            category_parser = CategoryDetailParser(category)
+            filename = get_product_urls_filepath(category.category_name, category.subcategory_name)
+            with CSVWriter(filename, Url) as writer:
+                writer.writerows(category_parser.parse())
 
-                for url in reader2:
-                    url = Url(**url)
-                    product_parser = ProductParser(url)
-                    writer.writerow(product_parser.parse())
+
+    # with open(CATEGORIES_PATH, "r") as f:
+    #     reader = csv.DictReader(f, delimiter="\t")
+    #
+    #     for category in reader:
+    #         urls_filename = get_product_urls_filepath(category["category_name"], category["subcategory_name"])
+    #         filename = get_products_filepath(category["category_name"], category["subcategory_name"])
+    #         with open(urls_filename, "r") as f2, CSVWriter(filename, Product) as writer:
+    #             reader2 = csv.DictReader(f2, delimiter="\t")
+    #
+    #             for url in reader2:
+    #                 url = Url(**url)
+    #                 product_parser = ProductParser(url)
+    #                 writer.writerow(product_parser.parse())

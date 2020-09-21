1970aaafc481603339cad98ca5e6a0fbf9c080ea
Юнусов Рустам Рустамович <R.Yunusov@vitebsk.energo.net>
2020-09-21 12:10:02 +0300

    Added parser for parsing products from category detail page

--- src/parsers.py
+++ src/parsers.py
@@ -4,7 +4,7 @@ import requests
 from abc import ABCMeta, abstractmethod
 from lxml import html
 
-from utils import make_url
+from utils import make_url, get_product_id_from_url
 from browsers import ChromeBrowser, FirefoxBrowser, IeBrowser
 from models import Category, Product, Url
 
@@ -119,11 +119,11 @@ class ProductParser(Parser, GetPageViaBrowserMixin):
         self.__url = url
 
     def _get_url(self):
-        return make_url(self.__url.url)
+        return make_url(self.__url.url, g="y")
 
     def __get_id_from_url(self):
         url = self._get_url()
-        return url.split("/")[-1].split(".")[0]
+        return get_product_id_from_url(url)
 
     def parse(self):
         text = self._get_page()
@@ -139,4 +139,70 @@ class ProductParser(Parser, GetPageViaBrowserMixin):
         tmp = tree.xpath(self.PRICE_XPATH)
         price = tmp[0] if tmp else None
 
-        return Product(id=id, description=description, number_of_orders=number_of_orders, price=price)
+        url = self.__url.url
+
+        product = Product(id=id, description=description, number_of_orders=number_of_orders, price=price, url=url)
+
+        logger.debug(product)
+
+        return product
+
+
+class ProductFromCategoryDetailParser(Parser, GetPageViaBrowserMixin):
+    PRODUCT_XPATH = "//ul[@class='list-items']/div/li[@class='list-item']"
+    DESCRIPTION_XPATH = ".//div[@class='hover-help']/div[@class='item-title-wrap']/a/text()"
+    NUMBER_OF_ORDERS_XPATH = ".//div[@class='item-sale-wrap']/div[@class='sale-info with-star']/span[@class='sale-value']/a/text()"
+    PRICE_XPATH = ".//div[@class='item-price-wrap']/div/span[@class='price-current']/text()"
+    URL_XPATH = ".//div[@class='product-img']/div[@class='place-container']/a/@href"
+
+    def __init__(self, category):
+        self.__category = category
+        self.__page = 1
+        self.__repeat = 0
+
+    def _get_url(self):
+        return make_url(self.__category.url, page=self.__page, g="y")
+
+    def _next_page(self):
+        self.__page += 1
+        self.__repeat = 0
+
+    def _check_repeat(self):
+        return self.__repeat < 5
+
+    def _next_repeat(self):
+        self.__repeat += 1
+
+    def parse(self):
+        while True:
+            text = self._get_page()
+            tree = html.fromstring(text)
+
+            products_tree = tree.xpath(self.PRODUCT_XPATH)
+
+            if not products_tree:
+                if self._check_repeat():
+                    self._next_repeat()
+                    continue
+                break
+
+            for idx, product_tree in enumerate(products_tree):
+                description = product_tree.xpath(self.DESCRIPTION_XPATH)[0]
+
+                tmp = product_tree.xpath(self.NUMBER_OF_ORDERS_XPATH)
+                number_of_orders = tmp[0] if tmp else None
+                
+                tmp = product_tree.xpath(self.PRICE_XPATH)
+                price = tmp[0] if tmp else None
+                
+                url = product_tree.xpath(self.URL_XPATH)[0]
+
+                id = get_product_id_from_url(url)
+
+                product = Product(id=id, description=description, number_of_orders=number_of_orders, price=price, url=url)
+
+                logger.debug(f"{self.__page} {idx} {product}")
+
+                yield product
+
+            self._next_page()

e36b40c9e669439975b52e8c733cb029be674305
Юнусов Рустам Рустамович <R.Yunusov@vitebsk.energo.net>
2020-09-22 13:41:41 +0300

    Added cookie for captcha to browser

--- src/browsers.py
+++ src/browsers.py
@@ -1,5 +1,6 @@
 import time
-from selenium.webdriver import Chrome, Firefox, Ie, ActionChains
+from selenium.webdriver import Chrome, Firefox, Ie, ActionChains, ChromeOptions
+from selenium.common.exceptions import NoSuchElementException
 
 from settings import (
     CHROMEDRIVER_PATH, 
@@ -27,16 +28,40 @@ class Browser:
     def login(self):
         self._browser.get(self.ALIEXPRESS_LOGIN_URL)
         time.sleep(SLEEP_TIME_FOR_LOGIN)
+    
+    def add_cookie_for_captcha(self):
+        x5sec = input("Введите x5sec cookie:\n")
+        self._browser.add_cookie({
+            "name": "x5sec",
+            "value": x5sec,
+            "domain": "aliexpress.ru",
+            "path": "/",
+        })
 
     def get_page(self, url):
         self._browser.get(url)
         # Scroll down to load all products
         total_height = int(self._browser.execute_script("return document.body.scrollHeight"))
-        
+
         for height in range(1, total_height, total_height // 10):
             self._browser.execute_script(f"window.scrollTo(0,{height})")
             time.sleep(SLEEP_TIME_FOR_PAGE_SCROLLING)
         time.sleep(SLEEP_TIME_FOR_NEXT_PAGE)
+
+        # Не работает
+        # try:
+        #     move = ActionChains(self._browser)
+        #     slider = self._browser.find_element_by_xpath("//span[@id='nc_1_n1z']")
+        #     move.click_and_hold(slider).move_by_offset(500, 0).release().perform()
+        #     time.sleep(5)
+        # except NoSuchElementException:
+        #     pass
+
+        try:
+            slider = self._browser.find_element_by_xpath("//span[@id='nc_1_n1z']")
+            self.add_cookie_for_captcha()
+        except NoSuchElementException:
+            pass
 
         return self._browser.page_source
 
@@ -45,7 +70,6 @@ class ChromeBrowser(Browser, metaclass=SingletonMetaclass):
     def __init__(self):
         self._browser = Chrome(executable_path=CHROMEDRIVER_PATH)
         self.login()
-
 
 
 class FirefoxBrowser(Browser, metaclass=SingletonMetaclass):
